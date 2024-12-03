fed8ff3d8e29efd85034a739434871e4711246de
Andrew G. Neumann <agneum14@gmail.com>
2024-12-03 13:36:58 -0500

    Complete day01 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,16 +1,16 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 use itertools::Itertools;
 
 struct Lists {
-    fst: Vec<u32>,
-    snd: Vec<u32>,
+    fst: Vec<u64>,
+    snd: Vec<u64>,
 }
 
 impl Lists {
     fn new(input: &str) -> Self {
         let (fst, snd) = input.lines().fold(
-            (Vec::<u32>::new(), Vec::<u32>::new()),
+            (Vec::<u64>::new(), Vec::<u64>::new()),
             |(mut fst, mut snd), x| {
                 let data = x.split_whitespace().collect::<Vec<_>>();
                 fst.push(data[0].parse().unwrap());
@@ -21,12 +21,26 @@ impl Lists {
         Self { fst, snd }
     }
 
-    fn total_distance(&self) -> u32 {
+    fn total_distance(&self) -> u64 {
         self.fst
             .iter()
             .sorted()
             .zip(self.snd.iter().sorted())
             .map(|(x, y)| x.abs_diff(*y))
+            .sum()
+    }
+
+    fn similarity_score(&self) -> u64 {
+        let frequencies = self
+            .snd
+            .iter()
+            .fold(HashMap::<u64, usize>::new(), |mut freqs, x| {
+                *freqs.entry(*x).or_default() += 1;
+                freqs
+            });
+        self.fst
+            .iter()
+            .map(|x| x * *frequencies.get(x).unwrap_or(&0) as u64)
             .sum()
     }
 }
@@ -35,6 +49,7 @@ pub fn run() {
     let input = read_to_string("inputs/day01.txt").unwrap();
     let lists = Lists::new(&input);
     println!("Total distance: {}", lists.total_distance());
+    println!("Similarity score: {}", lists.similarity_score());
 }
 
 #[cfg(test)]
@@ -47,5 +62,11 @@ mod tests {
     fn total_distance() {
         let lists = Lists::new(&read_to_string("inputs/day01_small.txt").unwrap());
         assert_eq!(11, lists.total_distance())
+    }
+
+    #[test]
+    fn similarity_score() {
+        let lists = Lists::new(&read_to_string("inputs/day01_small.txt").unwrap());
+        assert_eq!(31, lists.similarity_score())
     }
 }
