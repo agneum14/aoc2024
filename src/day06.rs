283f962882016bfaf22bf5275c5c352318ffd735
Andrew G. Neumann <agneum14@gmail.com>
2024-12-06 13:20:49 -0500

    Complete day 06 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,11 +1,14 @@
 use std::{
     collections::{HashMap, HashSet},
     fs::read_to_string,
+    isize,
 };
 
 struct Map {
     grid: HashMap<(isize, isize), Space>,
     start: (isize, isize),
+    y_max: isize,
+    x_max: isize,
 }
 
 impl Map {
@@ -25,38 +28,82 @@ impl Map {
                 grid.insert((y as isize, x as isize), space);
             }
         }
-        Self { grid, start }
+        let y_max = input.lines().count() as isize;
+        let x_max = input.lines().next().unwrap().chars().count() as isize;
+        Self {
+            grid,
+            start,
+            y_max,
+            x_max,
+        }
     }
 
     fn distinct_positions(&self) -> usize {
         let mut visited = HashSet::from([self.start]);
-        let (y, x) = self.start;
-        self.walk(&mut visited, Direction::North, y, x)
+        self.walk(&mut visited, Direction::North, self.start)
+    }
+
+    fn different_obstructions(&self) -> usize {
+        (0..self.y_max)
+            .flat_map(|y| (0..self.x_max).map(move |x| (y, x)))
+            .filter(|pos| *pos != self.start && self.grid.get(pos) != Some(&Space::Obstacle))
+            .map(|pos| self.cycles(pos))
+            .filter(|x| *x)
+            .count()
     }
 
     fn walk(
         &self,
         visited: &mut HashSet<(isize, isize)>,
         direction: Direction,
-        y: isize,
-        x: isize,
+        position: (isize, isize),
     ) -> usize {
-        if self.grid.get(&(y, x)).is_none() {
+        if self.grid.get(&position).is_none() {
             return visited.len();
         }
-        visited.insert((y, x));
+        visited.insert(position);
 
-        let facing = match direction {
+        let facing = Map::facing(direction, position);
+        if self.grid.get(&facing) == Some(&Space::Obstacle) {
+            return self.walk(visited, direction.rotate(), position);
+        }
+        return self.walk(visited, direction, facing);
+    }
+
+    fn cycles(&self, obstacle: (isize, isize)) -> bool {
+        return self.inner_cycles(&mut HashSet::new(), obstacle, Direction::North, self.start);
+    }
+
+    fn inner_cycles(
+        &self,
+        visited: &mut HashSet<((isize, isize), Direction)>,
+        obstacle: (isize, isize),
+        direction: Direction,
+        position: (isize, isize),
+    ) -> bool {
+        if self.grid.get(&position).is_none() {
+            return false;
+        }
+        if visited.contains(&(position, direction)) {
+            return true;
+        }
+        visited.insert((position, direction));
+
+        let facing = Map::facing(direction, position);
+        if self.grid.get(&facing) == Some(&Space::Obstacle) || facing == obstacle {
+            return self.inner_cycles(visited, obstacle, direction.rotate(), position);
+        }
+        self.inner_cycles(visited, obstacle, direction, facing)
+    }
+
+    fn facing(direction: Direction, position: (isize, isize)) -> (isize, isize) {
+        let (y, x) = position;
+        match direction {
             Direction::North => (y - 1, x),
             Direction::East => (y, x + 1),
             Direction::South => (y + 1, x),
             Direction::West => (y, x - 1),
-        };
-        if self.grid.get(&facing) == Some(&Space::Obstacle) {
-            return self.walk(visited, direction.rotate(), y, x);
         }
-        let (y, x) = facing;
-        return self.walk(visited, direction, y, x);
     }
 }
 
@@ -66,6 +113,7 @@ enum Space {
     Obstacle,
 }
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 enum Direction {
     North,
     East,
@@ -87,7 +135,8 @@ impl Direction {
 pub fn run() {
     let input = read_to_string("inputs/day06.txt").unwrap();
     let map = Map::new(&input);
-    println!("Distinct positions: {}", map.distinct_positions())
+    println!("Distinct positions: {}", map.distinct_positions());
+    println!("Different obstructions: {}", map.different_obstructions())
 }
 
 #[cfg(test)]
@@ -98,5 +147,23 @@ mod tests {
     fn distinct_positions() {
         let map = Map::new(&read_to_string("inputs/day06_small.txt").unwrap());
         assert_eq!(41, map.distinct_positions())
+    }
+
+    #[test]
+    fn positive_cycle() {
+        let map = Map::new(&read_to_string("inputs/day06_small.txt").unwrap());
+        assert_eq!(true, map.cycles((6, 3)))
+    }
+
+    #[test]
+    fn negative_cycle() {
+        let map = Map::new(&read_to_string("inputs/day06_small.txt").unwrap());
+        assert_eq!(false, map.cycles((6, 2)))
+    }
+
+    #[test]
+    fn different_obstructions() {
+        let map = Map::new(&read_to_string("inputs/day06_small.txt").unwrap());
+        assert_eq!(6, map.different_obstructions())
     }
 }
