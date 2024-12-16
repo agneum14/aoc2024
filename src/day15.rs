38ba6e33ccdfc878d678c718a0c8144eaf6a834f
Andrew G. Neumann <agneum14@gmail.com>
2024-12-15 22:04:44 -0500

    Complete day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::{collections::HashMap, fs::read_to_string};
 
 use itertools::Itertools;
@@ -50,35 +51,78 @@ impl Warehouse {
 
     fn shift(&mut self, direction: Direction) {
         let target = direction.target(self.robot);
-        if self.shift_inner(target, direction) {
+        if self.shift_inner(target, direction, false, false) {
+            let _ = self.shift_inner(target, direction, false, true);
             self.robot = target;
         }
     }
 
-    fn shift_inner(&mut self, pos: (isize, isize), direction: Direction) -> bool {
+    fn shift_inner(
+        &mut self,
+        pos: (isize, isize),
+        direction: Direction,
+        inner: bool,
+        shift: bool,
+    ) -> bool {
         let square = self.grid.get(&pos);
         match square {
             None => true,
             Some(&Square::Wall) => false,
             Some(&Square::Box) => {
                 let target = direction.target(pos);
-                let foo = self.shift_inner(target, direction);
-                if foo {
+                let foo = self.shift_inner(target, direction, false, shift);
+                if foo && shift {
                     self.grid.remove(&pos);
                     self.grid.insert(target, Square::Box);
+                }
+                foo
+            }
+            Some(&Square::LeftBox) | Some(&Square::RightBox) => {
+                let square = *square.unwrap();
+                let target = direction.target(pos);
+                let foo;
+                if inner {
+                    foo = self.shift_inner(target, direction, false, shift);
+                } else {
+                    let opposite = square.opposite();
+                    if direction == opposite {
+                        foo = self.shift_inner(target, direction, true, shift)
+                    } else {
+                        foo = self.shift_inner(target, direction, false, shift)
+                            && self.shift_inner(opposite.target(pos), direction, true, shift)
+                    }
+                }
+                if foo && shift {
+                    self.grid.remove(&pos);
+                    self.grid.insert(target, square);
                 }
                 foo
             }
         }
     }
 
-    fn gps_sum(&mut self) -> isize {
-        self.simulate();
+    fn gps_sum(&self) -> isize {
         self.grid
             .iter()
-            .filter(|(_, square)| **square == Square::Box)
+            .filter(|(_, square)| **square == Square::Box || **square == Square::LeftBox)
             .map(|(pos, _)| pos.0 * 100 + pos.1)
             .sum()
+    }
+
+    fn widen(&mut self) {
+        let mut new = HashMap::new();
+        for (pos, space) in self.grid.iter() {
+            let (fst, snd) = match space {
+                Square::Wall => (Square::Wall, Square::Wall),
+                Square::Box => (Square::LeftBox, Square::RightBox),
+                _ => unreachable!(),
+            };
+            let pos = (pos.0, pos.1 * 2);
+            new.insert(pos, fst);
+            new.insert((pos.0, pos.1 + 1), snd);
+        }
+        self.grid = new;
+        self.robot = (self.robot.0, self.robot.1 * 2)
     }
 }
 
@@ -86,9 +130,21 @@ impl Warehouse {
 enum Square {
     Wall,
     Box,
+    LeftBox,
+    RightBox,
 }
 
-#[derive(Debug, Clone, Copy)]
+impl Square {
+    fn opposite(&self) -> Direction {
+        match self {
+            Self::LeftBox => Direction::Right,
+            Self::RightBox => Direction::Left,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -110,8 +166,13 @@ impl Direction {
 
 pub fn run() {
     let input = read_to_string("inputs/day15.txt").unwrap();
-    let warehouse = Warehouse::new(&input);
-    println!("GPS sum: {}", warehouse.clone().gps_sum());
+    let mut warehouse = Warehouse::new(&input);
+    let mut wide = warehouse.clone();
+    wide.widen();
+    wide.simulate();
+    warehouse.simulate();
+    println!("GPS sum: {}", warehouse.gps_sum());
+    println!("Wide GPS sum: {}", wide.gps_sum());
 }
 
 #[cfg(test)]
@@ -123,6 +184,15 @@ mod tests {
     #[test]
     fn gps_sum() {
         let mut warehouse = Warehouse::new(&read_to_string("inputs/day15_small.txt").unwrap());
+        warehouse.simulate();
         assert_eq!(10092, warehouse.gps_sum())
+    }
+
+    #[test]
+    fn wide_gps_sum() {
+        let mut warehouse = Warehouse::new(&read_to_string("inputs/day15_small.txt").unwrap());
+        warehouse.widen();
+        warehouse.simulate();
+        assert_eq!(9021, warehouse.gps_sum())
     }
 }
