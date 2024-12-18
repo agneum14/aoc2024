f07294d1d6e52150f3907f55bfe7fbf3c5c147d6
Andrew G. Neumann <agneum14@gmail.com>
2024-12-18 01:30:18 -0500

    Complete day 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -10,10 +10,11 @@ struct Computer {
     width: isize,
     start: Point,
     exit: Point,
+    count: usize,
 }
 
 impl Computer {
-    fn new(input: &str, width: isize) -> Self {
+    fn new(input: &str, width: isize, count: usize) -> Self {
         let bytes = input
             .lines()
             .map(|line| {
@@ -30,6 +31,7 @@ impl Computer {
             width,
             start: (0, 0),
             exit: (width - 1, width - 1),
+            count,
         }
     }
 
@@ -53,10 +55,10 @@ impl Computer {
             .collect::<HashMap<_, _>>()
     }
 
-    fn steps(&self, count: usize) -> u32 {
-        let grid = self.drop_bytes(count);
+    fn steps(&self) -> u32 {
+        let grid = self.drop_bytes(self.count);
         let mut cache = HashMap::new();
-        self.steps_inner(self.start, 0, &mut cache, count, &grid);
+        self.steps_inner(self.start, 0, &mut cache, &grid);
         *cache.get(&self.exit).unwrap()
     }
 
@@ -65,7 +67,6 @@ impl Computer {
         p: Point,
         steps: u32,
         cache: &mut HashMap<Point, u32>,
-        count: usize,
         grid: &HashMap<Point, Space>,
     ) {
         let space = grid.get(&p);
@@ -80,8 +81,46 @@ impl Computer {
         cache.insert(p, steps);
         let (y, x) = p;
         for p in [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)] {
-            self.steps_inner(p, steps + 1, cache, count, grid);
+            self.steps_inner(p, steps + 1, cache, grid);
         }
+    }
+
+    fn reachable(&self, grid: &HashMap<Point, Space>) -> bool {
+        self.reachable_inner(self.start, &mut HashSet::new(), grid)
+    }
+
+    fn reachable_inner(
+        &self,
+        p: Point,
+        visited: &mut HashSet<Point>,
+        grid: &HashMap<Point, Space>,
+    ) -> bool {
+        let space = grid.get(&p);
+        if space == None || space == Some(&Space::Corrupted) || visited.contains(&p) {
+            return false;
+        }
+        if p == self.exit {
+            return true;
+        }
+        visited.insert(p);
+        let (y, x) = p;
+        [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)]
+            .into_iter()
+            .any(|p| self.reachable_inner(p, visited, grid))
+    }
+
+    fn first_byte(&self) -> Point {
+        let mut low = self.count;
+        let mut high = self.bytes.len();
+        while high - low > 1 {
+            let mid = (high + low) / 2;
+            if self.reachable(&self.drop_bytes(mid)) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        self.bytes[low]
     }
 }
 
@@ -94,8 +133,10 @@ enum Space {
 }
 
 pub fn run() {
-    let computer = Computer::new(&read_to_string("inputs/day18.txt").unwrap(), 71);
-    println!("Minimum steps: {}", computer.steps(1024))
+    let computer = Computer::new(&read_to_string("inputs/day18.txt").unwrap(), 71, 1024);
+    println!("Minimum steps: {}", computer.steps());
+    let fb = computer.first_byte();
+    println!("First byte: {},{}", fb.1, fb.0);
 }
 
 #[cfg(test)]
@@ -104,7 +145,13 @@ mod tests {
 
     #[test]
     fn minimum_steps() {
-        let computer = Computer::new(&read_to_string("inputs/day18_small.txt").unwrap(), 7);
-        assert_eq!(22, computer.steps(12))
+        let computer = Computer::new(&read_to_string("inputs/day18_small.txt").unwrap(), 7, 12);
+        assert_eq!(22, computer.steps())
+    }
+
+    #[test]
+    fn first_byte() {
+        let computer = Computer::new(&read_to_string("inputs/day18_small.txt").unwrap(), 7, 12);
+        assert_eq!((1, 6), computer.first_byte())
     }
 }
